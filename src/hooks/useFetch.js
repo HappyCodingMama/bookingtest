35ccbb93e6b834afde0dfe92c7bbfdb1a6dfa49d
YANG.J <67956944+HappyCodingMama@users.noreply.github.com>
2022-12-27 04:21:29 -0800

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const useFetch = (url) => {
+const baseUrl = 'https://bookingtestback.onrender.com';
+
+const useFetch = (baseUrl+ url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -10,7 +12,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await axios.get(url);
+        const res = await axios.get(baseUrl + url);
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -18,12 +20,12 @@ const useFetch = (url) => {
       setLoading(false);
     };
     fetchData();
-  }, [url]);
+  }, [baseUrl + url]);
 
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = await axios.get(url);
+      const res = await axios.get(baseUrl + url);
       setData(res.data);
     } catch (err) {
       setError(err);
